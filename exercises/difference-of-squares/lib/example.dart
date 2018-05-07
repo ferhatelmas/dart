71781e0d9757cdaf83ac6c7f4350c7d72be8d930
Stargator <Stargator@users.noreply.github.com>
2018-05-06 22:08:16 -0400

    dart-exercism: Update sdk constraints on exercises

--- exercises/difference-of-squares/lib/example.dart
+++ exercises/difference-of-squares/lib/example.dart
@@ -1,13 +1,13 @@
 import "dart:math" show pow;
 
 class DifferenceOfSquares {
-  int squareOfSum(int input) => pow(_sum(input), 2).toInt();
+  num squareOfSum(int input) => pow(_sum(input), 2);
 
-  int sumOfSquare(int input) => _range(input).map((i) => pow(i, 2)).reduce((r, i) => r + i).toInt();
+  num sumOfSquare(int input) => _range(input).map((i) => pow(i, 2)).reduce((r, i) => r + i);
 
-  int differenceOfSquares(int input) => squareOfSum(input) - sumOfSquare(input);
+  num differenceOfSquares(int input) => squareOfSum(input) - sumOfSquare(input);
 
-  int _sum(int input) => input * (input + 1) ~/ 2;
+  num _sum(int input) => input * (input + 1) ~/ 2;
 
-  List<int> _range(int length) => new List<int>.generate(length, (i) => i + 1);
+  List<num> _range(int length) => new List<num>.generate(length, (i) => i + 1);
 }
